f78e5a4c9f4e0b53d7fe68f49099e0b7ba5fa7bb
Jesus Vargas <jesusvarguer18@gmail.com>
2022-10-13 16:06:14 -0500

    File updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const $app = document.getElementById('app');
-const API = "https://api.escuelajs.co/api/v1/products?offset=0&limit=10"
+const API = "https://api.escuelajs.co/api/v1/products?offset=5&limit=10"
 
 const main = async () => {
   const response = await fetch(API);
@@ -10,16 +10,15 @@ const main = async () => {
       <article class="Card">
         <img src="${product.images[0]}">
         <h2>
-          ${product.title} <small>Precio $ ${product.price} </small>
+          ${product.title} <small>Precio $ ${product.price}</small>
         </h2>
       </article>
     `;
-  }).json('');
+  }).join('');
   let newItem = document.createElement("section");
   newItem.classList.add("Items");
   newItem.innerHTML = output;
   $app.appendChild(newItem);
-
 }
 
 main();
